854145dbbd9097b3ccae77dde99fb9080774b50a
HaoranDing <3616266449@qq.com>
2024-10-10 09:52:24 +0800

    新增七原则之一全部内容

--- src/main/java/com/lucifer/principle/DependecyInversion/improve.java
+++ src/main/java/com/lucifer/principle/DependecyInversion/improve.java
@@ -1,5 +1,8 @@
 package com.lucifer.principle.DependecyInversion;
 
+//有三种实现依赖倒转的方式，个人理解：把接口注入到类里面的三种方式
+
+
 public class improve {
     public static void main(String[] args) {
         Person person = new Person();
@@ -35,3 +38,82 @@ class Person {
         System.out.println(receiver.getInfo());
     }
 }
+
+
+//三种方式如下：
+// 方式 1： 通过接口传递实现依赖
+
+// interface IOpenAndClose {
+// public void open(ITV tv); //抽象方法,接收接口
+
+// }
+//
+// interface ITV { //ITV 接口
+
+// public void play();
+// }
+//
+// class ChangHong implements ITV {
+//
+// @Override
+// public void play() {
+// System.out.println("长虹电视机，打开");
+// }
+//
+// }
+//// 实现接口
+
+// class OpenAndClose implements IOpenAndClose{
+// public void open(ITV tv){
+// tv.play();
+// }
+// }
+// 方式 2: 通过构造方法依赖传递
+
+// interface IOpenAndClose {
+
+// public void open(); //抽象方法
+
+// }
+// interface ITV { //ITV 接口
+
+// public void play();
+// }
+// class OpenAndClose implements IOpenAndClose{
+// public ITV tv; //成员
+
+// public OpenAndClose(ITV tv){ //构造器
+
+// this.tv = tv;
+// }
+// public void open(){
+// this.tv.play();
+// }
+// }
+//// 方式 3 , 通过 setter 方法传递
+//
+//interface IOpenAndClose {
+//    public void open(); // 抽象方法
+//
+//    public void setTv(ITV tv);
+//}
+//interface ITV { // ITV 接口
+//
+//    public void play();
+//}
+//
+//class OpenAndClose implements IOpenAndClose {
+//    private ITV tv;
+//    public void setTv(ITV tv) {
+//        this.tv = tv;
+//    }
+//    public void open() {
+//        this.tv.play();
+//    }
+//}
+//class ChangHong implements ITV {
+//    @Override
+//    public void play() {
+//        System.out.println("长虹电视机，打开");
+//    }
+//}
---
+++ src/main/java/com/lucifer/principle/demeter/improve/Demeter1.java
@@ -0,0 +1,101 @@
+package com.lucifer.principle.demeter.improve;
+
+import java.util.ArrayList;
+import java.util.List;
+//等效客户端
+public class Demeter1 {
+
+	public static void main(String[] args) {
+		SchoolManager schoolManager = new SchoolManager();
+		schoolManager.printAllEmployee(new CollegeManager());
+
+	}
+
+}
+//雇员
+class Employee {
+	private String id;
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	//返回总部员工
+
+}
+//学校总部员工
+class CollegeEmployee {
+	private String id;
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+}
+
+//管理类
+class CollegeManager {
+	//返回学院的所有员工
+	public List<CollegeEmployee> getAllEmployee() {
+		List<CollegeEmployee> list = new ArrayList<CollegeEmployee>();
+		for (int i = 0; i < 10; i++) {
+			CollegeEmployee emp = new CollegeEmployee();//增加了10个员工到list集合
+			emp.setId("员工id= " + i);
+			list.add(emp);
+		}
+		return list;
+	}
+
+	public List<Employee> getEmployee() {
+		List<Employee> list = new ArrayList<Employee>();
+
+		for (int i = 0; i < 5; i++) {
+			Employee emp = new Employee();
+			emp.setId("总部员工id= " + i);
+			list.add(emp);
+		}
+		return list;
+	}
+
+	//输出学院员工的信息
+	public void printEmployee(){
+		System.out.println("------------学校总部员工------------");
+		for (Employee e : this.getEmployee()) {
+			System.out.println(e.getId());
+		}
+
+	}
+}
+
+//学校管理类
+//直接朋友：Employee；CollegeManager
+//todo:间接朋友：CollegeEmployee，违反了迪米特法则（陌生类作为了局部变量）
+class SchoolManager {
+
+	//解决方案：将输出员工id的方法封装进原本的类之中
+
+
+
+	//输出学校总部和学校员工的id
+	void printAllEmployee(CollegeManager sub) {
+
+		sub.printEmployee();
+		
+		List<CollegeEmployee> list1 = sub.getAllEmployee();
+		System.out.println("------------学校员工------------");
+		for (CollegeEmployee e : list1) {
+			System.out.println(e.getId());
+		}
+
+
+	}
+
+
+}
